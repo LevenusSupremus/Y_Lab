4db3703c9caf2de8ad62f87fae67a71932e68782
SLCKDM <43304005+SLCKDM@users.noreply.github.com>
2021-10-14 22:52:59 +0300

    Add files via upload

--- RTTT.py
+++ RTTT.py
@@ -1,7 +1,8 @@
 import random
 
-# Создаём поле, которое можно изменять по ходу игры
+
 def create_board(size):
+    """ Создает поле размером :size: """
     board = []
     for i in range(size):
         row = []
@@ -10,10 +11,12 @@ def create_board(size):
         board.append(row)
     return board
 
-# Вывод игрового поля
+
 def print_board(board):
+    """ Выводит поле :board: """
     print(" ", end='')
-    for x in range(len(board)): print(f'{x}'.rjust(2), end="")
+    for x in range(len(board)):
+        print(f'{x}'.rjust(2), end="")
     print()
     for ind, i in enumerate(board):
         print(ind, end=' ')
@@ -21,37 +24,51 @@ def print_board(board):
             print(j, end=" ")
         print()
 
-# Ход игрока за X
+
 def turn(player):
+    """ В данной игре игрок :player: ходит только за крестик 
+        В цикле происходит валидация вводимых игроком координат
+        (Являются ли числом? Две ли коориднаты даны? Свободна ли клетка?)
+    """
     print(f'Сейчас ходит {player}')
     while True:
         turn_input = input("Выберите ячейку (Напишите координаты слитно XY):")
-        if str(turn_input).isnumeric() and len(str(turn_input)) == 2 and not board[int(turn_input[0])][int(turn_input[1])] != '-': break
+        if str(turn_input).isnumeric() \
+                and len(str(turn_input)) == 2 \
+                and not board[int(turn_input[0])][int(turn_input[1])] != '-':
+            break
     x, y = turn_input
     board[int(x)][int(y)] = player
 
-# Ход компьютера за O
+
 def computer_turn(board):
+    """ Имитация хода компьютера. Просматриваются все возможные
+        ходы, добавляются наиболее оптимальные (Хотя я даже не могу
+        придумать, что означает "Оптимальный ход" в данном случае...)
+    """
     possible_moves = []
     for row in range(len(board)):
         for col in range(len(board)):
-            if board[row][col] == "-"                         \
-            or board[row-1][col] != ("X" or "O")              \
-            or board[row][col-1] != ("X" or "O")              \
-            or board[row-1][col-1] != ("X" or "O")            \
-            or board[row][col+1] != ("X" or "O")              \
-            or board[row+1][col] != ("X" or "O")              \
-            or board[row+1][col+1] != ("X" or "O"):
+            if board[row][col] == "-"                                 \
+                    or board[row-1][col] != ("X" or "O")              \
+                    or board[row][col-1] != ("X" or "O")              \
+                    or board[row-1][col-1] != ("X" or "O")            \
+                    or board[row][col+1] != ("X" or "O")              \
+                    or board[row+1][col] != ("X" or "O")              \
+                    or board[row+1][col+1] != ("X" or "O"):
                 possible_moves.append((row, col))
     return possible_moves[random.randrange(len(possible_moves))]
 
-# Проверка наличия 5 ходов в один ряд (Горизонталь/вертикаль/диагональ)
+
 def is_player_lose(player):
+    """ Проверка, есть ли 5 в ряд для любого игрока :player:
+        в любом из направлений
+    """
     lose = 0
-    n = len(board)
+    size = len(board)
     # Горизонталь
-    for i in range(n):
-        for j in range(n):
+    for i in range(size):
+        for j in range(size):
             if board[i][j] == player:
                 lose += 1
                 if lose == 5:
@@ -59,30 +76,30 @@ def is_player_lose(player):
             else:
                 lose = 0
     # Вертикаль
-    for i in range(n):
-        for j in range(n):
+    for i in range(size):
+        for j in range(size):
             if board[j][i] == player:
                 lose += 1
                 if lose >= 5:
                     return lose
             else:
                 lose = 0
-    # Диагонали 
-    for i in range(-(n - 1), n):
-        for j in range(n):
+    # Диагонали
+    for i in range(-(size - 1), size):
+        for j in range(size):
             row, col = j, i + j
-            if 0 <= row < n and 0 <= col < n:
+            if 0 <= row < size and 0 <= col < size:
                 if board[row][col] == player:
                     lose += 1
                     if lose >= 5:
                         return lose
                 else:
                     lose = 0
-                    
-    for i in range(-(n - 1), n):
-        for j in range(n):
+
+    for i in range(-(size - 1), size):
+        for j in range(size):
             row, col = j, i + j
-            if 0 <= row < n and 0 <= col < n:
+            if 0 <= row < size and 0 <= col < size:
                 if board[row][10 - col - 1] == player:
                     lose += 1
                     if lose >= 5:
@@ -90,8 +107,11 @@ def is_player_lose(player):
                 else:
                     lose = 0
 
-# Проверка заполненности поля. Если возвращает True - завершает игру
+
 def board_filled():
+    """ Проверка зполненности поля 
+        Если выдаёт True - геймовер
+    """
     for row in board:
         for item in row:
             if item == '-':
@@ -100,16 +120,24 @@ def board_filled():
 
 
 def main():
+    """ Процесс и логика игры
+        - Сначала инициализируем ходы и поле;
+        Далее, в цикле:
+    >--> - Выводим поле;
+    |    - Проверяем, кому отдавать ход - Игроку(X) / Компьютеру(O). Отдаём;
+    |    - Проверяем есть ли 5 в ряд и свободное место в поле;
+    ^--< По новой.
+    """
     turns = 0
     global board
     board = create_board(10)
-    
+
     while True:
+        print_board(board)
+
         player = "X" if turns % 2 == 0 else "O"
         turns += 1
-        
-        print_board(board)
-        
+
         if player == "X":
             turn(player)
         else:
@@ -123,6 +151,7 @@ def main():
         elif board_filled():
             print('Ничья!')
             break
+
 
 if __name__ == "__main__":
     main()
